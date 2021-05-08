cb26387b2c348a153063d65dfe45b1339e457630
= <=>
2021-05-08 16:27:19 -0500

    admin draft (not complete)

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ from flaskext.mysql import MySQL
 from flask import session, flash
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_cors import CORS
-
-
+import os
+from werkzeug.utils import secure_filename
+#UPLOAD_FOLDER = '/static/images'
+#ALLOWED_EXTENSIONS = {'jpg'}
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app = Flask(__name__)
 
 
@@ -23,10 +26,205 @@ app.secret_key = 'secret key can be anything!'
 
 print("\n\n\n\n")
 
+
+
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/admin/', methods=['POST', 'GET'])
+def admin():
+    if request.method == "GET":
+        return render_template("admin.html")
+
+@app.route('/admin/users', methods=['POST', 'GET'])
+def admin_users():
+    if request.method == "GET":
+        connected_to_database = 0
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            connected_to_database == 1
+            cursor.execute("SELECT id, username FROM user")
+            data = cursor.fetchall()
+            return render_template("admin_users.html", data=data)
+        except Exception as e:
+            print("exception:", str(e))
+            return render_template('admin_users.html')
+        finally:
+            if connected_to_database == 1:
+                cursor.close()
+                conn.close()
+
+@app.route('/admin/cars', methods=['POST', 'GET'])
+def admin_cars():
+    if request.method == "GET":
+        connected_to_database = 0
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            connected_to_database == 1
+            # join car table with images on VIN number
+            cursor.execute("SELECT car.VIN, car.Make, car.Model, car.Color, car.Year, car.Seats, car.Price_Per_Day,"
+             " image.image_number FROM car JOIN image ON image.CAR_VIN = car.VIN GROUP BY car.VIN")
+            data = cursor.fetchall()
+                    
+            return render_template("admin_cars.html", data=data)
+        except Exception as e:
+            print("exception:", str(e))
+            return render_template('admin_cars.html')
+        finally:
+            if connected_to_database == 1:
+                cursor.close()
+                conn.close()
+@app.route('/edit/<vin>', methods=['POST', 'GET'])
+def admin_edit_cars(vin):
+    if request.method == "GET":
+        connected_to_database = 0
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            connected_to_database = 1
+
+            #selecting images 
+            cursor.execute("SELECT image_number FROM image WHERE CAR_VIN = %s", (vin))
+            images = cursor.fetchall()
+            cursor.close()
+            conn.close()
+            connected_to_database = 0
+
+            #selecting cars
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            connected_to_database = 1
+            cursor.execute("SELECT Make, Model, Color, Year, Seats, Price_Per_Day FROM car WHERE VIN = %s", (vin))
+            car = cursor.fetchone()
+            return render_template("admin_edit.html", images=images, vin = vin, car = car)
+        except Exception as e:
+            print("exception:", str(e))
+            return render_template('admin_edit.html')
+        finally:
+            if connected_to_database == 1:
+                cursor.close()
+                conn.close()
+                connected_to_database = 0
+    elif request.method == "POST":
+        car_make = request.form['fmake']
+        car_model = request.form['fmodel']
+        car_color = request.form['fcolor']
+        car_year = request.form['fyear']
+        car_seats = request.form['fseats']
+        car_price = request.form['fprice']
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            connected_to_database = 1
+            cursor.execute("UPDATE car SET Make = %s, Model = %s, Color = %s, Year = %s, Seats = %s, Price_Per_Day = %s"
+            "WHERE VIN = %s", (car_make, car_model, car_color, car_year, car_seats, car_price, vin))
+            conn.commit()
+            return redirect("/admin/cars")
+        except Exception as e:
+            print("exception:", str(e))
+            return render_template('admin_edit.html')
+        finally:
+            if connected_to_database == 1:
+                cursor.close()
+                conn.close()
+                connected_to_database = 0
+
+
+
+
+
+@app.route('/delete/<vin>', methods=['POST', 'GET'])
+def admin_delete_cars(vin):
+    if request.method == "GET":
+        connected_to_database = 0
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            connected_to_database = 1
+
+            #selecting images 
+            cursor.execute("SELECT image_number FROM image WHERE CAR_VIN = %s", (vin))
+            images = cursor.fetchall()
+            cursor.close()
+            conn.close()
+            connected_to_database = 0
+
+            #selecting cars
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            connected_to_database = 1
+            cursor.execute("SELECT Make, Model, Color, Year, Seats, Price_Per_Day FROM car WHERE VIN = %s", (vin))
+            car = cursor.fetchone()
+            return render_template("admin_delete.html", images=images, vin = vin, car = car)
+        except Exception as e:
+            print("exception:", str(e))
+            return render_template('admin_delete.html')
+        finally:
+            if connected_to_database == 1:
+                cursor.close()
+                conn.close()
+                connected_to_database = 0
+    elif request.method == "POST":
+        if request.form["toDelete"] == 'yesDelete':
+               #_user = session.get('user')
+                conn = mysql.connect()
+                cursor = conn.cursor()
+
+                #first delete the foreign key
+                cursor.execute("DELETE FROM image WHERE CAR_VIN = %s", (vin))
+                conn.commit()
+                cursor.close()
+                conn.close()
+
+
+                conn = mysql.connect()
+                cursor = conn.cursor()
+
+                #now delete the actual car
+                cursor.execute("DELETE FROM car WHERE VIN = %s", (vin))
+                conn.commit()
+                cursor.close()
+                conn.close()
+
+
+                return redirect('/admin/cars')
+        #no delete
+        else:
+            return redirect('/admin/cars')
+
+@app.route('/admin/add', methods=['POST', 'GET'])
+def admin_add_cars():
+    if request.method == "GET":
+            return render_template("admin_add.html")
+    elif request.method == "POST":
+        car_make = request.form['fmake']
+        car_model = request.form['fmodel']
+        car_color = request.form['fcolor']
+        car_year = request.form['fyear']
+        car_seats = request.form['fseats']
+        car_price = request.form['fprice']
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            connected_to_database = 1
+            cursor.execute("UPDATE car SET Make = %s, Model = %s, Color = %s, Year = %s, Seats = %s, Price_Per_Day = %s"
+            "WHERE VIN = %s", (car_make, car_model, car_color, car_year, car_seats, car_price, vin))
+            conn.commit()
+            return redirect("/admin/cars")
+        except Exception as e:
+            print("exception:", str(e))
+            return render_template('admin_edit.html')
+        finally:
+            if connected_to_database == 1:
+                cursor.close()
+                conn.close()
+                connected_to_database = 0
+            
 @app.route('/logout')
 def logout():
     session.pop('user',None)
@@ -300,4 +498,4 @@ def savedList():
 
 
 if __name__ == "__main__":
-    app.run()   
+    app.run(debug=True)   
